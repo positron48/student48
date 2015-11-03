dde18a242dd450a3a5d66a0fc3b60908d283e4b3
root <root@vps212106.ovh.net>
2015-11-03 08:08:30 +0100

    изменения с боевого

--- include/database.php
+++ include/database.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbWorker = new PDO('mysql:host=localhost;dbname=student48;charset=utf8', 'root', 'positron48rus');
+    $dbWorker = new PDO('mysql:host=localhost;dbname=student48;charset=utf8', 'student48', 'positron48rus');
 } catch (PDOException $e) {
     print "Ошибка подключения к базе данных: " . $e->getMessage() . "<br/>";
     die();
