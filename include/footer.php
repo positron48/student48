87ef17abcbd2b8676be68e65187a8e1c7c913d3f
Anton Filatov <filatov.anton@pochta.ru>
2015-11-04 18:34:18 +0300

    task #23 оптимизация

--- include/footer.php
+++ include/footer.php
@@ -30,6 +30,12 @@
 				<?}?>
 			</div>    
 		</div>
+
+		<script src="/js/jquery.form.js"></script>
+		<script src="/js/bootstrap.js"></script>
+		<link rel="stylesheet" href="/css/style.css">
+
+
 		<script>
 			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
