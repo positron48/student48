b94bd1a9f07b6726d7f6fe9f0218045fa2d46d5a
Anton Filatov <filatov.anton@pochta.ru>
2016-01-26 22:19:03 +0300

    task #20 сфинкс тест

---
+++ ajax/sphinxsearch.php
@@ -0,0 +1,23 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/sphinxapi.php');
+
+// Создаем объект клиента для Sphinx API
+$sphinx = new SphinxClient();
+
+// Подсоединяемся к Sphinx-серверу
+$sphinx->SetServer('localhost', 3312);
+
+// Совпадение по любому слову
+$sphinx->SetMatchMode(SPH_MATCH_ANY);
+
+// Результаты сортировать по релевантности
+$sphinx->SetSortMode(SPH_SORT_RELEVANCE);
+
+// Результат по запросу (* - использование всех индексов)
+$result = $sphinx->Query($_REQUEST['q'], '*');
+
+if($result){
+    echo '<pre>';
+    print_r($result);
+    echo '</pre>';
+}
