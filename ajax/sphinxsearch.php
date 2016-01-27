549e2ee4a6562ca572e32118dbb0f8fe2548b68f
Anton Filatov <filatov.anton@pochta.ru>
2016-01-27 22:54:10 +0300

    task #20 вывод найденных материалов

--- ajax/sphinxsearch.php
+++ ajax/sphinxsearch.php
@@ -1,27 +1,41 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/database.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/sphinxapi.php');
 
-// Создадим объект - клиент сфинкса и подключимся к нашей службе
 $cl = new SphinxClient();
 $cl->SetServer( "127.0.0.1", 9312 );
 
-// Собственно поиск
-$cl->SetMatchMode( SPH_MATCH_ANY  ); // ищем хотя бы 1 слово из поисковой фразы
-
+$cl->SetMatchMode( SPH_MATCH_ANY  );
 $cl->setIndexWeights (["title_material"=>40,"title_predmet"=>10]);
-
 $resultMaterials = $cl->Query($_REQUEST['q'],"student48_index_materials"); // поисковый запрос
-if ( $resultMaterials === false ) {
-    echo "Query failed: " . $cl->GetLastError() . ".\n"; // выводим ошибку если произошла
+$materialIds = [];
+if ( $resultMaterials !== false ) {
+    $materialIds = array_keys($resultMaterials['matches']);
+    //берем только первые 5 элементов
+    $materialIds = array_slice($materialIds,0,5);
 }
+
 $cl->setIndexWeights (["title_news"=>50,"introtext"=>30, "fullcontent"=>10]);
 $resultNews = $cl->Query($_REQUEST['q'],"student48_index_news"); // поисковый запрос
-if ( $resultNews === false ) {
-    echo "Query failed: " . $cl->GetLastError() . ".\n"; // выводим ошибку если произошла
+$newsIds = [];
+if ( $resultNews !== false ) {
+    $newsIds = array_keys($resultNews['matches']);
+    //берем только первые 5 элементов
+    $newsIds = array_slice($newsIds,0,5);
 }
-echo "<pre>Materials:\n";
-print_r($resultMaterials);
-echo "</pre>";
-echo "<pre>News:\n";
-print_r($resultNews);
-echo "</pre>";
+
+$arMaterials = [];
+if(count($materialIds)>0) {
+    $qMarks = str_repeat('?,', count($materialIds) - 1) . '?';
+    $queryMaterials = "SELECT M.id, M.title_material, M.metakey, M.predmetid, M.link, M.filesize, M.downloads, M.dateadd,
+	P.title_predmet, P.title_predmet_english, P.semestr WHERE M.id IN ($qMarks) LIMIT :min, :count";
+    $dbMaterials = $dbWorker->prepare($queryMaterials);
+    $dbMaterials->execute($materialIds);
+
+    while ($material = $dbMaterials->fetch()) {
+        $arMaterials[$material['id']] = $material;
+    }
+    echo "<pre>";
+    print_r($arMaterials);
+    echo "</pre>";
+}
